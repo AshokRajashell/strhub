bef0adce3ad9fd9017fa001a037477671e11a1f9
AshokRajashell <107853454+AshokRajashell@users.noreply.github.com>
2023-06-28 22:30:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author_email="A.Palaniswamy@gmail.com",
     description=DESCRIPTION,
     packages=find_packages(),
-    package_data={"strhub":["configs/*.yaml","configs/model/*"]},
+    package_data={"strhub":["configs/*.yaml","configs/model/*","configs/charset/*.yaml","configs/experiment/*.yaml"]},
     install_requires=[],
     keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
     classifiers=[
